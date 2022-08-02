d7e14328e93861955f6e4ad23680fd71a9395a9c
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-02 19:13:54 +0300

    create index.js in ui

---
+++ src/components/UI/index.js
@@ -0,0 +1,7 @@
+//Простой способ импорта ui компонентов, для того чтобы не использовать каждый раз импорт обьектов
+
+import MyButton from "@/components/UI/MyButton";
+
+export default [
+    MyButton
+]
