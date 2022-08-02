3ddf1243cdc1a5229b83331ca222ffe4480fe02d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-02 17:04:46 +0300

    create PostItem.vue

---
+++ src/components/PostItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post">
+    <div>
+      <strong>
+        Название: <strong>{{post.title}}</strong>
+      </strong>
+    </div>
+    <div>
+      <strong>
+        Описание: <strong>{{post.body}}</strong>
+      </strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostItem"
+}
+</script>
+
+<style scoped>
+
+</style>
