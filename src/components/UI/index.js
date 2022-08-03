0f8c72849ba8ead61cb3c7f079015c4b713e5d91
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-03 12:40:02 +0300

    created MyDialog.vue and imported into index.js

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,8 +2,10 @@
 
 import MyButton from "@/components/UI/MyButton";
 import MyInput from "@/components/UI/MyInput";
+import MyDialog from "@/components/UI/MyDialog";
 
 export default [
     MyButton,
-    MyInput
+    MyInput,
+    MyDialog
 ]
