c3a71046130c958287bfad5e5fb82fd22650388a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-05 20:08:50 +0300

    removed from index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,6 @@
 import {createStore} from "vuex";
 
 export default createStore({
-    //стэйт это значения по умолчанию доступные в любом месте приложения
-    state: {
-        likes: 2,
-        isAuth: false,
-    },
-    //геттерс позволяют изменять значения стэйта
-    getters: {
-        doubleLikes(state) {
-            return state.likes * 2
-        }
-    },
-    mutations: {
-        incrementLikes(state) {
-            state.likes += 1
-        },
-        decrementLikes(state) {
-            state.likes -= 1
-        }
-    },
-    actions: {
-
-    },
     modules: {
 
     }
