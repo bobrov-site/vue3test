58501d239903c5e7e56483d27825317936a1bc65
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-05 14:17:35 +0300

    created item MyNav.vue

---
+++ src/components/UI/MyNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navigation">
+    <ul>
+      <li>
+        <router-link to="/posts">Посты</router-link>
+      </li>
+      <li>
+        <router-link to="/about">О приложении</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MyNav"
+}
+</script>
+
+<style scoped>
+.navigation {
+  margin-top: 15px;
+}
+.navigation ul {
+  display: flex;
+}
+.navigation ul li {
+  margin-right: 15px;
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,12 +1,15 @@
 <template>
 <div>
   <h1>Добро пожаловать</h1>
+  <MyNav/>
 </div>
 </template>
 
 <script>
+import MyNav from "@/components/UI/MyNav";
 export default {
-  name: "Main"
+  name: "Main",
+  components: {MyNav}
 }
 </script>
 
