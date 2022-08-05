b00dc2ec364390a083abd678ab79d44c3f828eb6
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-05 14:32:40 +0300

    added logo to Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
+  <!--         используется для вставки ссылки в кнопку $router.push('/ссылка')-->
+  <!--        в другом случае используется <router-link>-->
   <nav class="navigation">
+    <div class="logo">
+      <my-button @click="$router.push('/')">
+        Vue 3
+      </my-button>
+    </div>
     <ul>
       <li>
-<!--         используется для вставки ссылки в кнопку $router.push('/ссылка')-->
-<!--        в другом случае используется <router-link>-->
         <my-button @click="$router.push('/posts')">
           Посты
         </my-button>
@@ -29,6 +34,9 @@ export default {
   width: 100%;
   background: green;
   padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .navigation ul {
   display: flex;
@@ -37,8 +45,13 @@ export default {
 .navigation ul li {
   margin-right: 15px;
 }
-.navigation ul li a {
+.navigation ul li {
   text-decoration: none;
+  color: black;
+  background-color: white;
+}
+.logo button {
+  background-color: brown;
   color: white;
 }
 </style>
