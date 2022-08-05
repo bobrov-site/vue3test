2d83dc116bc088807c96df821d8f5d03c4a5da8e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-05 17:26:25 +0300

    created directives bundle

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import App from "./App";
 import componets from '@/components/UI/index';
 import router from "@/router/router";
 import VIntersection from "@/directives/VIntersection";
+import directives from "@/directives";
 
 const app = createApp(App)
 
 componets.forEach(component => {
     app.component(component.name, component)
 })
-
+directives.forEach(directive => {
+    app.directive(directive.name, directive)
+})
 app.directive('intersection', VIntersection);
 
 app.use(router).mount('#app')
